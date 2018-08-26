ee421b52220fa91198d53e304f1a83e55ba9ba48
Victor Felipe <victorfelipe1608@gmail.com>
2018-08-26 02:03:08 -0300

    Inclusão do Bootstrap

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, url_for, redirect
 from flask_sqlalchemy import SQLAlchemy
 from flask_security import Security, SQLAlchemyUserDatastore, UserMixin, RoleMixin, login_required
+from werkzeug.security import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI']='postgresql://root:root@localhost/brlab-base'
@@ -14,7 +15,7 @@ db = SQLAlchemy(app)
 #Define Models
 
 roles_users = db.Table('roles_users',
-db.Column('user_id', db.Integer(), db.ForeignKey('user.id')),
+db.Column('user_id', db.Integer, db.ForeignKey('user.id')),
 db.Column('role_id', db.Integer(), db.ForeignKey('role.id')))
 
 class Role(db.Model, RoleMixin):
@@ -24,43 +25,73 @@ class Role(db.Model, RoleMixin):
 
 class User(db.Model, UserMixin): #UserMixin
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(255), unique=True)
     email = db.Column(db.String(255), unique=True)
     password = db.Column(db.String(255))
     active = db.Column(db.Boolean())
     confirmed_at = db.Column(db.DateTime())
     roles = db.relationship('Role', secondary=roles_users, backref=db.backref('users',lazy='dynamic'))
 
-    def __init__(self, username, email, password):#,, active, confirmed_at
-        self.username = username
+    def __init__(self, email, password, active):#,, active, confirmed_at
         self.email = email
-        self.password = encrypt_password(password)
-        # self.active = active
-        # self.confirmed_at = confirmed_at
+        self.password = password
+        self.active = active
 
     def __repr__(self):
-        return '<User %r>' % self.username
-
+        return '<User %r>' % self.email
+#
+# FLask Security
+#
 user_datastore = SQLAlchemyUserDatastore(db, User, Role)
 security = Security(app, user_datastore)
 
 # @app.before_first_request
 # def create_user():
 #     db.create_all()
-#     user_datastore.create_user(username='admin', email='admin@admin.com', password='admin')
+#     user = User(email='root@root.com', password='root', active=1)
+#     db.session.add(user)
 #     db.session.commit()
+    # user_datastore.create_user(email='admin@admin.com', password='admin')
+    # db.session.commit()
 
 @app.route('/')
 def index():
-    return render_template("add_user.html")
-    myUser = User.query.all()
-    oneItem = User.query.filter_by(email='admin').all()
-    return render_template("add_user.html", myUser=myUser, oneItem=oneItem)
+    return render_template("index.html")
 
-@app.route('/profile/<username>')
-def profile(username):
-    user = User.query.filter_by(username = username).first()
+@app.route('/profile/<email>')
+@login_required
+def profile(email):
+    user = User.query.filter_by(email = email).first()
     return render_template("profile.html", user=user)
+
+
+@app.route('/begin')
+def begin():
+    return render_template("contact.html");
+
+@app.route('/labs')
+def labs():
+    return render_template("about.html");
+
+@app.route('/publications')
+def publications():
+    return render_template("about.html");
+
+@app.route('/projects')
+def projects():
+    return render_template("about.html");
+
+@app.route('/partners')
+def partners():
+    return render_template("about.html");
+
+@app.route('/about')
+def about():
+    return render_template("about.html");
+
+@app.route('/contact')
+def contact():
+    return render_template("contact.html");
+
 
 @app.route('/post_user',methods=['POST'])
 def post_user():
@@ -68,6 +99,13 @@ def post_user():
     db.session.add(user)
     db.session.commit()
     return redirect(url_for('index'))
+
+# @app.route('/post_role',methods=['POST'])
+# def post_role():
+#     role = Role(request.form['name'], request.form['description'])
+#     db.session.add(role)
+#     db.session.commit()
+#     return redirect(url_for('index'))
 
 if __name__ == "__main__":
     app.run()
--- models/tables.py
+++ models/tables.py
@@ -1,34 +1,34 @@
-from app import debug
-
-class User(db.Model):
-    __tablename__ = "users"
-
-    id = db.Column(db.Integer, primary_key = True)
-    username = db.Column(db.String(100), unique=True)
-    email = db.Column(db.String(100), unique=True)
-    password = db.Column(db.String(100))
-    name = db.Column(db.String(100))
-
-    def __init__(self, username, email, password, name):
-        self.username = username
-        self.email = email
-        self.password = password
-        self.name = name
-
-    def __repr__(self):
-        return "<User %r>" % self.username
-
-class Role(db.Model):
-    __tablename__ = "roles"
-    id = db.Column(db.Integer, primary_key = True)
-    name = db.Column(db.String(100), unique = True)
-    description = db.Column(db.String(200))
-
-    def __init__(self, name, description):
-        self.name = name
-        self.description = description
-
-
-roles_users = db.Table('roles_users',
-        db.Column('user_id', db.Integer, db.ForeignKey('user.id')),
-        db.Column('role_id', db.Integer, db.ForeignKey('role.id')))
+# from app import debug
+#
+# class User(db.Model):
+#     __tablename__ = "users"
+#
+#     id = db.Column(db.Integer, primary_key = True)
+#     username = db.Column(db.String(100), unique=True)
+#     email = db.Column(db.String(100), unique=True)
+#     password = db.Column(db.String(100))
+#     name = db.Column(db.String(100))
+#
+#     def __init__(self, username, email, password, name):
+#         self.username = username
+#         self.email = email
+#         self.password = password
+#         self.name = name
+#
+#     def __repr__(self):
+#         return "<User %r>" % self.username
+#
+# class Role(db.Model):
+#     __tablename__ = "roles"
+#     id = db.Column(db.Integer, primary_key = True)
+#     name = db.Column(db.String(100), unique = True)
+#     description = db.Column(db.String(200))
+#
+#     def __init__(self, name, description):
+#         self.name = name
+#         self.description = description
+#
+#
+# roles_users = db.Table('roles_users',
+#         db.Column('user_id', db.Integer, db.ForeignKey('user.id')),
+#         db.Column('role_id', db.Integer, db.ForeignKey('role.id')))
